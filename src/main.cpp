47385117a0aa1f2da9ecb4d5bbc0dd6b19aa66ad
hwj021 <96523209+hwj021@users.noreply.github.com>
2023-09-17 14:06:29 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,25 @@ int main(void){
     const int screenwidth = 800;
     const int screenheight = 800;
 
-    InitWindow(screenwidth, screenheight, "Big Juicy Window");
+    InitWindow(screenwidth, screenheight, "Okno");
+
+    Vector2 ballPosition = { screenwidth/2, screenheight/2 };
 
     SetTargetFPS(60);
     
     while (!WindowShouldClose())
+    
     {
+        if (IsKeyDown(KEY_RIGHT)){ ballPosition.x = ballPosition.x + 2;}
+        if (IsKeyDown(KEY_LEFT)){ ballPosition.x = ballPosition.x - 2;}
+        if (IsKeyDown(KEY_UP)){ ballPosition.y = ballPosition.y - 2;}
+        if (IsKeyDown(KEY_DOWN)){ ballPosition.y = ballPosition.y + 2;}
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
-        DrawCircle(400, 400, 150, BROWN);
+
+        ClearBackground(ORANGE);
+
+        DrawCircleV(ballPosition, 100, YELLOW);
 
         EndDrawing();
     }
